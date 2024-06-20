a5e42f6a8da315933c85772205dabd438609cf24
Rahul Saini <rs5778882@gmail.com>
2024-06-20 10:18:10 +0530

    Start End-to-End MLOps project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The NYC Trip Duration Predictor is a machine learning model that aims to predict the expected duration of a trip within New York City. By analyzing various factors such as the origin and destination locations, time of day, day of the week, and historical traffic patterns, the model can provide users with a reliable estimate of how long their journey will take.',
+    author='Rahul Saini',
+    license='',
+)
